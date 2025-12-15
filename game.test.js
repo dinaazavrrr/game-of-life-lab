f421205d88a95e04fb21876de416ab11376295ff
Diana <dianalutsyuk4347@gmail.com>
2025-12-15 20:37:43 +0100

    Add failing test

--- game.test.js
+++ game.test.js
@@ -3,3 +3,7 @@ const gameOfLife = require('./game');
 test('test setup works', () => {
   expect(gameOfLife()).toBe('ok');
 });
+
+test('should fail', () => {
+    expect(1 + 1).toBe(5); //помилка
+});
